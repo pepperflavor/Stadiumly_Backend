67c051552e9587e6e6fc3e2d20aaa6466e585cb9
pepperflavor <j8747j@gmail.com>
2025-06-04 11:24:07 +0900

    도커 설정파일

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as nodemailer from 'nodemailer';
+import * as sgMail from '@sendgrid/mail';
 
 @Injectable()
-export class MailService {
-  private readonly transporter: nodemailer.Transporter;
-  constructor(private readonly config: ConfigService) {
-    // const transport = {
-    //     SES : new
-    // }
+export class MailService implements OnModuleInit {
+  constructor(private readonly config: ConfigService) {}
+  onModuleInit() {
+    const apiKEY = this.config.get<string>('SEND_GRID_MAILER_API_KEY');
+    if (apiKEY) {
+      sgMail.setApiKey(apiKEY);
+    } else {
+      throw new Error('SEND_GRID_MAILER_API_KEY is not defined');
+    }
   }
+
+  // async sendVerificationMail(to: string, token: string) {
+  //   const from = this.config.get<string>()
+  // }
 }
